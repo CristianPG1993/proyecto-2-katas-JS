de62469ff5a9e4c2ae69115036dee47b82b60db4
CristianPG1993 <cristian.panos.gallego@gmail.com>
2025-12-16 11:28:15 +0100

    Añadir ejercicio 3

---
+++ ejercicios-basicos-avanzados/ejercicio-03.js
@@ -0,0 +1,38 @@
+// 1. Multiplica 10 por 5 y muestra el resultado mediante console
+
+const multiplicacion = 10 * 5;
+console.log("Ejercicio 1:");
+console.log("El resultado de multiplicar 10 por 5 es: " + multiplicacion);
+
+
+// 2. Divide 10 por 2 y muestra el resultado en un console
+
+const division = 10 / 2;
+console.log("Ejercicio 2:");
+console.log("El resultado de dividir 10 por 2 es: " + division);
+
+// 3. Muestra mediante un console el resto de dividir 15 por 9
+
+const resto = 15 % 9;
+console.log("Ejercicio 3:");
+console.log("El resto de dividir 15 por 9 es: " + resto);
+
+// 4.  Usa el correcto operador de asignación que resultará en o = 15, 
+// teniendo dos variables p = 10 y j = 5
+
+let p = 10;
+let j = 5;
+let o = p;
+o += j;
+console.log("Ejercicio 4:");
+console.log("El valor de o es: " + o);
+
+// 5. Usa el correcto operador de asignación que resultará en i = 50,
+// teniendo dos variables c = 10 y m = 5.
+
+let c = 10;
+let m = 5;
+let i = c;
+i *= m;
+console.log("Ejercicio 5:");
+console.log("El valor de i es: " + i);
