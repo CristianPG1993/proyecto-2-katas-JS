9c7ab26eda98fa042a5e361f267d1db13d31b4b0
CristianPG1993 <cristian.panos.gallego@gmail.com>
2025-12-16 15:40:10 +0100

    Añadir ejercico 10 y 11 con mejoras

--- ejercicios-basicos-avanzados/ejercicio-10.js
+++ ejercicios-basicos-avanzados/ejercicio-10.js
@@ -1,33 +1,22 @@
-//Calcular promedio mezclado: Crea una función que reciba por parámetro un array 
-// y cuando es un valor number lo sume y de lo contrario cuente la longitud del string 
-// y lo sume.
+//Calcular un promedio es una tarea extremadamente común, 
+// así que prueba a implementar esa funcionalidad en la siguiente función.
 
-//Es un poco locura, pero podremos ejercitar nuestra lógica con este ejercicio.
+//Puedes usar este array para probarla:
 
-//Puedes usar este array para probar tu función:
+const numbers = [12, 21, 38, 5, 45, 37, 6];
+function average(numberList) {
 
-const mixedElements = [
-  6,
-  1,
-  "Marvel",
-  1,
-  "hamburguesa",
-  "10",
-  "Prometeo",
-  8,
-  "Hola mundo",
-];
-function averageWord(list) {
+    // Manejo del caso de lista vacía
+    if (numberList.length === 0) {
+        return 0;
+    }
+  
     let sum = 0;
 
-    for (let i = 0; i < list.length; i++) {
-        if (typeof list[i] === "number") {
-            sum += list[i];
-        } else if (typeof list[i] === "string") {
-            sum += list[i].length;
-        }   
+    for (let i = 0; i < numberList.length; i++) {
+        sum += numberList[i];
     }
-    return sum;
-}
+    return sum/numberList.length;
 
-console.log(averageWord(mixedElements));
+}
+console.log(average(numbers));
