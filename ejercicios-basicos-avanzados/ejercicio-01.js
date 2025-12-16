a58745c9912007c0dc426edeaf99c04278471e52
CristianPG1993 <cristian.panos.gallego@gmail.com>
2025-12-16 10:54:39 +0100

    Ejercicio 1: variables y operaciones básicas

---
+++ ejercicios-basicos-avanzados/ejercicio-01.js
@@ -0,0 +1,19 @@
+// 1. Define tu superhéroe favorito
+let miSuperHeroeFavorito = "Hulk";
+
+// 2. Define un número
+let numeroFavorito = 50;
+
+// 3. Define las dimensiones
+let altura = 5;
+let longitud = 10;
+
+// 4. Calcular suma
+let suma = altura + longitud;
+
+// 5. Imprimir resultados
+console.log("Mi superhéroe favorito es: " + miSuperHeroeFavorito);
+console.log("Mi número favorito es: " + numeroFavorito);
+console.log("La altura es: " + altura);
+console.log("La longitud es: " + longitud);
+console.log("La suma de altura y longitud es: " + suma);
